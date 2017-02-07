637c20d2d59d60bbc13550a5e0c8c457fb2ed9c3
Branislav Remen <branislavremen@outlook.com>
2017-02-07 12:46:54 +0100

    add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AboutComponent }  from './components/about/about.component';
 import { NavbarComponent }  from './components/navbar/navbar.component';
 import { SearchComponent }  from './components/search/search.component';
 
+import { routing }  from './app.router';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, routing ],
   declarations: [
       AppComponent,
       AboutComponent,
